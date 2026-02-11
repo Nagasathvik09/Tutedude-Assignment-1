533f1846292b15e913b1c8365eaedf7b62321185
Naga sathvik <nagasathviksamkula@gmail.com>
2026-02-11 08:23:46 +0530

    Add files via upload

---
+++ Assignment-1a.py
@@ -0,0 +1,7 @@
+#To perform mathematical operations
+n1=int(input("enter the first number:"))
+n2=int(input("enter the second number:"))
+print("Addition:",n1+n2)
+print("Subtraction:",n1-n2)
+print("Multiplication:",n1*n2)
+print("Division:",n1/n2)
