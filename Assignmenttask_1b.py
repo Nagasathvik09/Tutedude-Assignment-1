522b920f24fe8ed1dfe1c593b7b5a678ef449882
Naga sathvik <nagasathviksamkula@gmail.com>
2026-02-18 21:30:56 +0530

    Delete Assignmenttask_1b.py

--- Assignmenttask_1b.py
+++
@@ -1,4 +0,0 @@
-#To create a personalized greeting
-first_name=input("Enter your first name: ")
-last_name=input("Enter your last name: ")
-print('Hello,',first_name+' '+last_name,'Welcome to the Python program.')
