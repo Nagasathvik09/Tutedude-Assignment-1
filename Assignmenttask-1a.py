ee1511b25e86164c8f9c0a79ea327ff2e6c3635a
Naga sathvik <nagasathviksamkula@gmail.com>
2026-02-18 21:33:10 +0530

    Add files via upload

---
+++ Assignmenttask-1a.py
@@ -0,0 +1,15 @@
+#To perform mathematical operations
+first_number=int(input("enter the first number:"))
+second_number=int(input("enter the second number:"))
+print("Addition:",first_number+second_number)
+print("Subtraction:",first_number-second_number)
+print("Multiplication:",first_number*second_number)
+if second_number!=0:
+    print("Division:", first_number / second_number)
+else:
+    print('Division is not possible as denominator is 0')
+
+
+
+
+
